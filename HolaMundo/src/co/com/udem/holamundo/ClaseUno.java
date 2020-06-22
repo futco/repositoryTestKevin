b758765c81cc4b6ff50c5f5a97b796b5df158abf
pcala@tsgroup.com.co <pauline cala>
2020-06-22 17:44:05 -0500

    cree una clase nueva

---
+++ HolaMundo/src/co/com/udem/holamundo/ClaseUno.java
@@ -0,0 +1,11 @@
+package co.com.udem.holamundo;
+
+public class ClaseUno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Esta es mi claseee");
+	}
+
+}
